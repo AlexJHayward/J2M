ed296883a7719811de3a00ae7e761151ca644082
Alex Hayward <alexjohnhayward@gmail.com>
2020-08-14 19:52:50 +0100

    added minification stuffs

---
+++ minify.js
@@ -0,0 +1,10 @@
+const babel = require("@babel/core");
+const fs = require("fs");
+
+const code = fs.readFileSync("./src/J2M.js").toString();
+const minified = babel.transform(code, {
+	presets: ["minify"],
+	comments: false
+});
+
+fs.writeFileSync("./J2m.min.js", minified.code);
